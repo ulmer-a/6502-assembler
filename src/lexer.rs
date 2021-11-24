c6d22e3ab0d827c6e46f2c80f2223c988e16bf91
Alexander Ulmer <ulmer@student.tugraz.at>
2021-11-24 20:06:50 +0100

    resolve warnings

--- src/lexer.rs
+++ src/lexer.rs
@@ -5,20 +5,20 @@ pub use asm_tokens::AsmToken;
 
 mod asm_lexer_tests;
 
-struct AsmLexer<'a> {
+pub struct AsmLexer<'a> {
     lexer: logos::Lexer<'a, AsmToken>,
     last_token: AsmToken,
 }
 
 impl<'a> AsmLexer<'a> {
-    fn new(source: &'a str) -> AsmLexer {
+    pub fn new(source: &'a str) -> AsmLexer {
         AsmLexer {
             lexer: AsmToken::lexer(source),
             last_token: AsmToken::Error,
         }
     }
 
-    fn next_token(&mut self) -> AsmToken {
+    pub fn next_token(&mut self) -> AsmToken {
         let token = match self.lexer.next() {
             Some(token) => token,
             None => AsmToken::Error,
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-mod lexer;
+use std::{env, fs};
 
-fn main() {}
+mod lexer;
+use lexer::{AsmLexer};
+
+fn main() {
+    let filename = env::args().skip(1).next().unwrap();
+    let source = fs::read_to_string(filename).unwrap();
+
+    let mut lexer = AsmLexer::new(&source);
+    lexer.next_token();
+}
